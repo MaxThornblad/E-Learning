044ae9be87df273bf8cc7710f5d0e35f416cfda7
Max Thörnblad <max.thornblad@hotmail.com>
2022-09-14 13:38:02 +0200

    Module 2 Part 14: Giving Types to Axios

--- client/src/actions/agent.ts
+++ client/src/actions/agent.ts
@@ -1,18 +1,18 @@
 import axios, { AxiosResponse } from "axios"
-
+import {Course} from "../models/course"
 axios.defaults.baseURL = "http://localhost:5000/api";
 
-const responseBody = (response: AxiosResponse) => response.data;
+const responseBody = <T> (response: AxiosResponse<T>) => response.data;
 
 const requests = {
-    get: (url: string) => axios.get(url).then(responseBody),
+    get: <T>(url: string) => axios.get<T>(url).then(responseBody),
     post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
     put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
     del: (url: string) => axios.delete(url).then(responseBody),
 };
 
 const Courses = {
-    list: () => requests.get("/courses")
+    list: () => requests.get<Course[]>("/courses")
 }
 
 const agent = {
