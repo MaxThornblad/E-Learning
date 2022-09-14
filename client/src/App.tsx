17592cffcd5aa1a889f1a23eedf0cb0f036b94d8
Max Thörnblad <max.thornblad@hotmail.com>
2022-09-14 12:33:57 +0200

    Module 2 Part 11: Creating Navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
-import './App.css';
 import DetailPage from './pages/DetailPage';
 import Homepage from './pages/Homepage';
 import LoginPage from './pages/Login';
+import Navigation from "./components/Navigation";
 
 function App() {
-  return <>
+  return (
+  <>
+  <Navigation/>
     <Switch>
       <Route exact path="/" component={Homepage}/>
       <Route exact path="/login" component={LoginPage}/>
       <Route exact path="/detail" component={DetailPage}/>
     </Switch>
-  </>;
+  </>
+  );
+  
 }
 
 export default App;
