fe3489449ec26b93b33153399246b4e55c1862c4
Max Thörnblad <max.thornblad@hotmail.com>
2022-09-15 12:50:31 +0200

    Module 3 Part 5: Adding Configuration to our Migration

--- Infrastructure/Migrations/StoreContextModelSnapshot.cs
+++ Infrastructure/Migrations/StoreContextModelSnapshot.cs
@@ -43,12 +43,18 @@ namespace Infrastructure.Migrations
                         .HasColumnType("INTEGER");
 
                     b.Property<string>("Image")
+                        .IsRequired()
                         .HasColumnType("TEXT");
 
                     b.Property<string>("Instructor")
+                        .IsRequired()
                         .HasColumnType("TEXT");
 
                     b.Property<string>("Language")
+                        .IsRequired()
+                        .HasColumnType("TEXT");
+
+                    b.Property<DateTime>("LastUpdated")
                         .HasColumnType("TEXT");
 
                     b.Property<string>("Level")
@@ -58,15 +64,18 @@ namespace Infrastructure.Migrations
                         .HasColumnType("REAL");
 
                     b.Property<decimal>("Rating")
-                        .HasColumnType("TEXT");
+                        .HasColumnType("decimal(18,1)");
 
                     b.Property<int>("Students")
                         .HasColumnType("INTEGER");
 
                     b.Property<string>("SubTitle")
+                        .IsRequired()
                         .HasColumnType("TEXT");
 
                     b.Property<string>("Title")
+                        .IsRequired()
+                        .HasMaxLength(150)
                         .HasColumnType("TEXT");
 
                     b.HasKey("Id");
