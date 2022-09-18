50b015627185dfcd8d805c0ad32dff2d9801d7e4
Max Thörnblad <max.thornblad@hotmail.com>
2022-09-18 18:07:41 +0200

    Module 4 Part 6: Using Specification Methods in Controllers

--- API/Controllers/CoursesController.cs
+++ API/Controllers/CoursesController.cs
@@ -5,6 +5,7 @@ using API.Dto;
 using AutoMapper;
 using Entity;
 using Entity.Interfaces;
+using Entity.Specifications;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -23,7 +24,8 @@ namespace API.Controllers
 
         public async Task<ActionResult<List<CourseDto>>> GetCourses()
         {
-            var courses = await _repository.ListAllAsync();
+            var spec = new CoursesWithCategoriesSpecification();
+            var courses = await _repository.ListWithSpec(spec);
             return Ok(_mapper.Map<IReadOnlyList<Course>, IReadOnlyList<CourseDto>>(courses));
         }
 
@@ -31,7 +33,8 @@ namespace API.Controllers
 
         public async Task<ActionResult<CourseDto>> GetCourse(Guid id)
         {
-            var course = await _repository.GetByIdAsync(id);
+            var spec = new CoursesWithCategoriesSpecification(id);
+            var course = await _repository.GetEntityWithSpec(spec);
 
             return _mapper.Map<Course, CourseDto>(course);
         }
