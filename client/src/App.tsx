48093575d2472b79c007d06af48b0939c849254b
Max Thörnblad <max.thornblad@hotmail.com>
2022-09-24 15:09:40 +0200

    Modul 9 Part 5: Refactoring App to Use Redux Toolkit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,51 +1,49 @@
+import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import 'antd/dist/antd.css';
+//import './App.css';
 import './sass/main.scss';
-import DetailPage from './pages/DetailPage';
-import Homepage from './pages/Homepage';
-import LoginPage from './pages/Login';
 import Navigation from './components/Navigation';
+import 'antd/dist/antd.css';
+import HomePage from './pages/Homepage';
+import Login from './pages/Login';
+import DetailPage from './pages/DetailPage';
 import Categories from './components/Categories';
 import CategoryPage from './pages/CategoryPage';
 import DescriptionPage from './pages/DescriptionPage';
 import BasketPage from './pages/BasketPage';
-import { useStoreContext } from './context/StoreContext';
-import { useEffect } from 'react';
 import agent from './actions/agent';
+import { useAppDispatch } from './redux/store/configureStore';
+import { setBasket } from './redux/slice/basketSlice';
 
 function App() {
-
-  const {setBasket} = useStoreContext();
+  const dispatch = useAppDispatch();
 
   function getCookie(name: string) {
     return (
-      document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || ''
+      document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() ||
+      ''
     );
   }
 
   useEffect(() => {
-    const clientId = getCookie("clientId")
-    
-    if(clientId) {
+    const clientId = getCookie('clientId');
+    if (clientId) {
       agent.Baskets.get()
-      .then((response) => {
-        setBasket(response)
-      })
-      .catch((error => console.log(error)))
+        .then((basket) => dispatch(setBasket(basket)))
+        .catch((error) => console.log(error));
     }
-  }, [setBasket]);
-
+  }, [dispatch]);
   return (
     <>
       <Navigation />
       <Route exact path="/" component={Categories} />
       <Switch>
-        <Route exact path="/" component={Homepage} />
+        <Route exact path="/" component={HomePage} />
         <Route exact path="/course/:id" component={DescriptionPage} />
-        <Route exact path="/category/:id" component={CategoryPage} />
-        <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/detail" component={DetailPage} />
         <Route exact path="/basket" component={BasketPage} />
+        <Route exact path="/category/:id" component={CategoryPage} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/detail" component={DetailPage} />
       </Switch>
     </>
   );
