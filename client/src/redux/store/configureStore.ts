50e96afbbfa2ec59e45c50faf0e6ed57a9378b04
Max Thörnblad <max.thornblad@hotmail.com>
2022-09-24 15:01:28 +0200

    Modul 9 Part 4: Creating Basket Slice

--- client/src/redux/store/configureStore.ts
+++ client/src/redux/store/configureStore.ts
@@ -1,16 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { loginSlice } from '../slice/loginSlice';
-
+import { configureStore } from '@reduxjs/toolkit'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import { basketSlice } from '../slice/basketSlice'
+import { loginSlice } from '../slice/loginSlice'
 
 export const store = configureStore({
-    reducer: {
-        login: loginSlice.reducer,
-    },
-});
+  reducer: {
+    login: loginSlice.reducer,
+    basket: basketSlice.reducer,
+  },
+})
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
 
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
