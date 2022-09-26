7095e214ffe51c0d00b88fac9d54104d3ed6d0eb
Max Thörnblad <max.thornblad@hotmail.com>
2022-09-27 00:12:24 +0200

    Module 11 Part 8: Making Clear Basket Endpoint

--- client/src/components/Checkout.tsx
+++ client/src/components/Checkout.tsx
@@ -8,6 +8,7 @@ import {
 import { Card, Form, Input, notification } from "antd";
 import { ChangeEvent, SyntheticEvent, useState } from "react";
 import { useHistory } from "react-router-dom";
+import agent from "../actions/agent";
 import { removeBasket } from "../redux/slice/basketSlice";
 import { useAppDispatch, useAppSelector } from "../redux/store/configureStore";
 import CheckoutSummary from "./CheckoutSummary";
@@ -51,6 +52,7 @@ const Checkout = () => {
                     message: "Your payment is successful!"
                 })
                 dispatch(removeBasket());
+                await agent.Baskets.clear();
                 setTimeout(() => {
                     history.push("/profile")
                 }, 1000)
