fc6cf2d6ae78bfa36411761a8fdd05da43be91ca
Max Thörnblad <max.thornblad@hotmail.com>
2022-09-30 10:49:18 +0200

    Module 13 Part 6: Create Sections Frontend

--- client/src/actions/agent.ts
+++ client/src/actions/agent.ts
@@ -4,7 +4,7 @@ import { Store } from "redux";
 import { Basket } from "../models/basket";
 import { Category } from "../models/category";
 import { Course, RegisterCourse } from "../models/course";
-import { Lecture } from "../models/lecture";
+import { Lecture, LectureDto } from "../models/lecture";
 import { PaginatedCourse } from "../models/paginatedCourse";
 import { Login, Register, User } from "../models/user";
 
@@ -90,9 +90,11 @@ const Users = {
 
 const Courses = {
   list: (params?: URLSearchParams) =>
-    requests.get<PaginatedCourse>("courses", params),
-  getById: (id: string) => requests.get<Course>(`courses/${id}`),
-  create: (data: RegisterCourse) => requests.post<string>('/courses', data)
+    requests.get < PaginatedCourse > ('courses', params),
+  getById: (id: string) => requests.get < Course > (`courses/${id}`),
+  create: (data: RegisterCourse) => requests.post < string > ('courses', data),
+  publish: (courseId: string) =>
+    requests.post < string > (`courses/publish/${courseId}`, {}),
 };
 
 const Categories = {
@@ -114,9 +116,16 @@ const Payments = {
 };
 
 const Lectures = {
-  getLectures: (courseId: string) => requests.get<Lecture>(`lectures/${courseId}`),
-  setCurrentLecture: (values: {lectureId: number, courseId: string}) => requests.put("lecutres/setCurrentLecture", values)
-}
+  getLectures: (courseId: string) =>
+    requests.get < Lecture > (`lectures/${courseId}`),
+  setCurrentLecture: (values: { lectureId: number, courseId: string }) =>
+    requests.put('lectures/setCurrentLecture', values),
+  create: (data: {
+    courseId: string,
+    sectionName: string,
+    lectures: LectureDto[],
+  }) => requests.post < string > ('lectures', data),
+};
 
 const agent = {
   Courses,
